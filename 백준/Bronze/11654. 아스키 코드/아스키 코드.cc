9a7084d4b49500de78b5aaaa28d286a35fe00dc8
gunboo <150986713+gunboo@users.noreply.github.com>
2024-07-17 13:31:43 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    char a;
+    cin >> a;
+
+    cout << static_cast<int>(a) << endl; //아스키코드로 변환하는것
+    //처음봄 ㅋㅋ
+    return 0;
+}
